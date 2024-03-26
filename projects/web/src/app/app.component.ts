7dbc22806960d9cdf1353bd7e33705f963324667
rgurgul <gurgul.robert@gmail.com>
2024-03-26 16:23:16 +0100

    x

--- projects/web/src/app/app.component.ts
+++ projects/web/src/app/app.component.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IMenu, MenuComponent, MyLibComponent } from '../../../my-lib/src/public-api';
+import { MainComponent } from './layout/components/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MyLibComponent, MenuComponent],
+  imports: [MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'web';
-  menus:IMenu[] = [
-    {path:'pages/items', name:'items'},
-    {path:'pages/workers', name:'workers'},
-  ]
+
 }
